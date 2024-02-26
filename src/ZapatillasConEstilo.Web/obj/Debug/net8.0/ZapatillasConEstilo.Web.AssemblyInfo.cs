21731e4787e14c0bfbaa071a89613c19702e9554
CoreDX1 <chrismaquis@gmail.com>
2024-02-26 13:29:05 -0300

    Agregado Refencias

--- src/ZapatillasConEstilo.Web/obj/Debug/net8.0/ZapatillasConEstilo.Web.AssemblyInfo.cs
+++ src/ZapatillasConEstilo.Web/obj/Debug/net8.0/ZapatillasConEstilo.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZapatillasConEstilo.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a432b141fc3995227ff97aa6df7e2a26a93f4bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078d25835acf9d1ac00b59c3a65a2ef9e2ce85e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZapatillasConEstilo.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZapatillasConEstilo.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
