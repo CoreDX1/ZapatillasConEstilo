2398f4b91f20de156e783a26cc511207c4af2dc4
CoreDX1 <chrismaquis@gmail.com>
2024-03-15 14:24:33 -0300

    Usando Qwik

---
+++ src/ZapatillasConEstilo.Web/AppClient/src/routes/contact/index.tsx
@@ -0,0 +1,6 @@
+import { component$ } from '@builder.io/qwik';
+import './index.css';
+ 
+export default component$(() => {
+  return <h1 class="title">Contact</h1>;
+});
---
+++ src/ZapatillasConEstilo.Web/AppClient/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi 👋</h1>
+      <p>
+        Can't wait to see what you build with qwik!
+        <br />
+        Happy coding.
+      </p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Bienvenido al Home",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
